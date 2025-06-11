d829fdda67275fb53128e6b7a8c169e34c321544
Gabriele Gonzato <gabrielegonzato04@gmail.com>
2025-06-11 17:13:37 +0200

    rimuovo index.html

--- mad-meteor/astro.config.mjs
+++ mad-meteor/astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
   output: 'static',
   base: '/PortfolioSetUp/', // usa il nome del repo
-  integrations: [github()],
 });
