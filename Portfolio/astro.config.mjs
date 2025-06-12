b9f2eaf6e39c7aaa16bdd3f4f4444881710be15b
Gabriele Gonzato <gabrielegonzato04@gmail.com>
2025-06-12 17:35:33 +0200

    sidebar menu

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+const repo = 'PortfolioSetUp';
 
 export default defineConfig({
+  integrations: [react(), tailwind()],
+  base: `/${repo}/`,
   output: 'static',
-  base: '/PortfolioSetUp/', 
 });
