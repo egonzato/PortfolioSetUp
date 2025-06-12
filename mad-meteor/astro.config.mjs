1d6cf3d03e309e54570e50f886164e3b6ef06471
Gabriele Gonzato <gabrielegonzato04@gmail.com>
2025-06-12 09:23:00 +0200

    clean repo

--- mad-meteor/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  output: 'static',
-  base: '/PortfolioSetUp/', // usa il nome del repo
-  srcDir: './mad-meteor',
-});
