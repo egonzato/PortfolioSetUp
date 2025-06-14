d91982312d82dc3c88ec564e62a1b0325bfba65c
Gabriele Gonzato <gabrielegonzato04@gmail.com>
2025-06-14 15:04:39 +0200

    prova deploy 2

--- Portfolio/src/settings.ts
+++ Portfolio/src/settings.ts
@@ -28,7 +28,7 @@ export const template = {
 	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
-    base: '' // Repository name starting with /
+    base: '/PortfolioSetUp/' // Repository name starting with /
 }
 
 export const seo = {
