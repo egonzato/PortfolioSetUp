e6e0e338cae0badf0ff74baf7ea26e29dc4dd9d8
egonzato <e.gonzato@amsterdamumc.nl>
2025-09-28 17:38:06 +0200

    Changed name of selected publications

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -117,7 +117,7 @@ export const publications = [
     authors: 'Elia Gonzato',
     journal: '',
     time: '2023',
-    contentHtml: '<a href="/PortfolioSetUp/Tesi_di_Gonzato_Elia___889721.pdf" target="_blank" class="text-blue-900 hover:text-accent transition-colors duration-200">MSc Thesis</a>',
+    contentHtml: '<a href="/PortfolioSetUp/Tesi_di_Gonzato_Elia___889721.pdf" target="_blank" class="text-blue-900 hover:text-accent transition-colors duration-200">Effect of transplant characteristics and complications (Master Thesis)</a>',
     abstract: 'Using data from 674 transplants in Stockholm (2015–2023), researchers assessed how donor type affects survival after haematopoietic stem cell transplantation. Recipients with mismatched related donors had poorer survival compared to those with identical sibling donors. Mediation analysis indicated that graft-versus-host disease only minimally explained these differences. The study underscores the continuing importance of finding compatible donors to improve outcomes.',
   },
   {
